d28a529597305ed1d29380b95075db51bdaa550e
Andrew Woo <seungminleader@gmail.com>
2024-02-27 14:58:13 -0500

    Rewrite + Basepath

--- page/next.config.mjs
+++ page/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: "/:path*",
+            destination: `/:path*`,
+          },
+          {
+            source: "/gameforge",
+            destination: `${GAMEFORGE_URL}/gameforge`,
+          },
+          {
+            source: "/gameforge/:path*",
+            destination: `${GAMEFORGE_URL}/gameforge/:path*`,
+          },
+        ];
+      },
+};
 
 export default nextConfig;
