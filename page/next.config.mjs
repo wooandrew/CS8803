b483f587c33a98b30cb1bf4bc05053ec7d99323c
Andrew Woo <seungminleader@gmail.com>
2024-02-27 16:30:08 -0500

    Fixed inconsistent spacing

--- page/next.config.mjs
+++ page/next.config.mjs
@@ -1,29 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: "/:path*",
-            destination: `/:path*`,
-          },
-          {
-            source: "/gameforge",
-            destination: `${process.env.GAMEFORGE_URL}/gameforge`,
-          },
-          {
-            source: "/gameforge/:path*",
-            destination: `${process.env.GAMEFORGE_URL}/gameforge/:path*`,
-          },
-          {
-            source: "/criticalcartography",
-            destination: `${process.env.CRITICALCARTOGRAPHY_URL}/criticalcartography`,
-          },
-          {
-            source: "/criticalcartography/:path*",
-            destination: `${process.env.CRITICALCARTOGRAPHY_URL}/criticalcartography/:path*`,
-          },
-        ];
-      },
+  async rewrites() {
+      return [
+        {
+          source: "/:path*",
+          destination: `/:path*`,
+        },
+        {
+          source: "/gameforge",
+          destination: `${process.env.GAMEFORGE_URL}/gameforge`,
+        },
+        {
+          source: "/gameforge/:path*",
+          destination: `${process.env.GAMEFORGE_URL}/gameforge/:path*`,
+        },
+        {
+          source: "/criticalcartography",
+          destination: `${process.env.CRITICALCARTOGRAPHY_URL}/criticalcartography`,
+        },
+        {
+          source: "/criticalcartography/:path*",
+          destination: `${process.env.CRITICALCARTOGRAPHY_URL}/criticalcartography/:path*`,
+        },
+      ];
+    },
 };
 
 export default nextConfig;
