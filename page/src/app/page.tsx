7786b8059b4ca55e576cdc1ff89acf90d7bdc3d4
Andrew Woo <seungminleader@gmail.com>
2024-02-29 22:05:26 -0500

    Fix lsquo

--- page/src/app/page.tsx
+++ page/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full justify-between font-mono text-sm lg:flex flex-col">
         <div className="mb-6">
-        Howdy. This is the landing page for my CS 8803 CDP (Spring '24) Projects @ GeorgiaTech
+        Howdy. This is the landing page for my CS 8803 CDP (Spring &lsquo;24) Projects @ GeorgiaTech
         <br></br>This page is under construction.
         </div>
 
